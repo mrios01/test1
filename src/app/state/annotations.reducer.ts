b6ea955b09f1b9df4c361c6df59a3dfb0121c7ff
mrios01 <m.angel.rios@gmail.com>
2021-04-12 14:39:20 -0500

    1 file modified

--- src/app/state/annotations.reducer.ts
+++ src/app/state/annotations.reducer.ts
@@ -1,33 +1,52 @@
 /* Actions */
-import { ADD_ANNOTATION ,REMOVE_ANNOTATION } from './annotations.actions';
+import { ADD_ANNOTATION, REMOVE_ANNOTATION } from "./annotations.actions";
 /* Models */
-import { IAnnotation } from '../shared/models/annotation.model';
+import { IAnnotation } from "../shared/models/annotation.model";
 /* Enums */
-import { Annotation } from '../shared/enums/annotation.enum';
+import { Annotation } from "../shared/enums/annotation.enum";
 
 export function annotationsReducer(state: IAnnotation[] = [], action) {
-	switch (action.type) {
+  switch (action.type) {
+    case ADD_ANNOTATION:
+      if (action.payload.type == Annotation.LINESTRING) {
+        state = [
+          action.payload,
+          ...state.filter(annot => {
+            return annot.type != Annotation.LINESTRING;
+          })
+        ];
+      }
 
-		case ADD_ANNOTATION:
+      if (action.payload.type == Annotation.POLYGON) {
+        state = [
+          action.payload,
+          ...state.filter(annot => {
+            return annot.type != Annotation.POLYGON;
+          })
+        ];
+      }
 
-			if(action.payload.type == Annotation.LINESTRING){
-				state = [action.payload, ...state.filter((annot) => { return annot.type != Annotation.LINESTRING})];
-			}
+      if (action.payload.type == Annotation.POINT) {
+        state = [action.payload, ...state];
+      }
 
-			if(action.payload.type == Annotation.POLYGON){
-				state = [action.payload, ...state.filter((annot) => { return annot.type != Annotation.POLYGON})];
-			}
+      if (action.payload.type == Annotation.PINS) {
+        state = [action.payload, ...state];
+      }
 
-			if(action.payload.type == Annotation.POINT){
-				state = [action.payload, ...state];
-			}
+      return state;
 
-			return state;
+    case REMOVE_ANNOTATION:
+      return [
+        ...state.filter(annot => {
+          return (
+            `${annot.id}${annot.type}` !=
+            `${action.payload.id}${action.payload.type}`
+          );
+        })
+      ];
 
-		case REMOVE_ANNOTATION:
-			return [...state.filter((annot) => { return `${annot.id}${annot.type}` != `${action.payload.id}${action.payload.type}` })];
-
-		default:
-			return state;
-		}
+    default:
+      return state;
+  }
 }
